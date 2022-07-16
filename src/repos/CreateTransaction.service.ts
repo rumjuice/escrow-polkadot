6af569f64a5c04226ac9dcab1362282471cbc39a
rumjuice <ramdhani@live.com>
2022-07-15 20:34:50 -0400

    add transaction response type

--- src/repos/CreateTransaction.service.ts
+++ src/repos/CreateTransaction.service.ts
@@ -1,14 +1,15 @@
+import { TransactionResponse } from "@ethersproject/abstract-provider";
 import { Transaction } from "../Model";
 import contract from "./Blockchain.provider";
 
-async function createTransaction(params: Transaction): Promise<any> {
-  const execute = contract.createTransaction(
+async function createTransaction(
+  params: Transaction
+): Promise<TransactionResponse> {
+  return await contract.createTransaction(
     params.productId,
     params.buyer,
     params.price
   );
-
-  return execute;
 }
 
 export default createTransaction;
